a55f7d00c93ecb4bca83b59787a3488772b1af85
Orendra Singh <orendrasingh@gmail.com>
2025-03-21 00:29:40 +0530

    Added postgresql

--- linkscreator/settings.py
+++ linkscreator/settings.py
@@ -39,9 +39,6 @@ ALLOWED_HOSTS = []
 
 
 
-
-
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -87,17 +84,30 @@ WSGI_APPLICATION = 'linkscreator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         "DB": os.getenv("DB_NAME"),
+#         'USER': os.getenv("DB_USERNAME"),
+#         'PASSWORD': os.getenv("DB_PASSWORD"),
+#         'HOST': os.getenv("DB_HOST"),
+#         'PORT': os.getenv("DB_PORT"),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        "DB": os.getenv("DB_NAME"),
+
+'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USERNAME"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT"),
     }
 }
+
 
 
 # Password validation
